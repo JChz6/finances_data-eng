d9175a7d6d63d602bf955f925194be0772078c69
Jchz6 <jesuschavez612@gmail.com>
2025-03-29 15:18:46 -0500

    Manejo de carga incremental mensual

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import pytz
 from google.cloud import bigquery, storage
 import functions_framework
 import logging
+from google.cloud.bigquery import QueryJobConfig
 
 
 utc_minus_5 = pytz.timezone('America/Lima')
@@ -29,6 +30,19 @@ def presup_to_bigquery(df, table_id):
     job = client.load_table_from_dataframe(df, table_id, job_config=job_config)
     job.result()
     logging.info(f"Cargado {job.output_rows} filas en {table_id}")
+
+#Elimina registros existentes del mes que va a ingresar
+def delete_old_data_from_bigquery(year_months, table_id):
+    client = bigquery.Client()
+
+    for year, month in year_months:
+        query = f"""
+        DELETE FROM `{table_id}`
+        WHERE EXTRACT(YEAR FROM fecha) = {year} AND EXTRACT(MONTH FROM fecha) = {month}
+        """
+        query_job = client.query(query, job_config=QueryJobConfig())
+        query_job.result()
+        logging.info(f"🗑️ Eliminados registros de {year}-{month} en {table_id}")
 
 
 #Cargar los registros
@@ -97,7 +111,14 @@ def handle_gcs_event(cloud_event):
             df['fecha_carga'] = datetime.now(pytz.utc).astimezone(utc_minus_5)
             df['fecha_carga'] = df['fecha_carga'].dt.tz_localize(None)
             
+            #  Extraer año-mes únicos
+            df["anio_mes"] = df["fecha"].apply(lambda x: (x.year, x.month))
+            unique_year_months = df["anio_mes"].drop_duplicates().tolist()
             
+            # Eliminar registros en BigQuery para esos año-mes
+            table_id = "big-query-406221.finanzas_personales.historico"
+            delete_old_data_from_bigquery(unique_year_months, table_id)
+
             # Convertir la columna 'comentario' a string, normalizar y buscar la cadena
             mask = (
                 df['comentario']
@@ -115,11 +136,9 @@ def handle_gcs_event(cloud_event):
                 .str.strip()
                 .astype(float)
                 )
-            
-            #df['dias_trabajados'] = pd.to_numeric(df['dias_trabajados'], errors='coerce')
-
-            logging.info("Datos transformados correctamente para el archivo .xlsx")
-            upload_to_bigquery(df, 'big-query-406221.finanzas_personales.historico')
+           
+            logging.info("✅ Datos transformados correctamente para el archivo .xlsx")
+            upload_to_bigquery(df.drop(columns=["anio_mes"]), 'big-query-406221.finanzas_personales.historico')
 
         elif file_name.endswith('.csv'):
             df = pd.read_csv(temp_file_path, delimiter = ';', encoding='latin1')
